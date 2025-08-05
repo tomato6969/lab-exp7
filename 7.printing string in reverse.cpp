e3e580cfa3c8e78bf91d244261ddfe56e80e9b64
Yashraj kaushik <88870393+tomato6969@users.noreply.github.com>
2025-08-05 16:00:27 +0530

    Add files via upload

---
+++ 7.printing string in reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+   char s []="Dsa using java";
+     int n=sizeof( s )/ sizeof( s [0]);
+    for(int i=n;i>=0;i--)
+    cout<<s [i];
+return 0;
+
+}
+
+Output
+avaj gnisu asD
